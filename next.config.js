ed5d81ead567fe182226d6e6d644340ab75e0be5
Shubham Verma <vshubham799@gmail.com>
2023-05-08 23:33:48 -0700

    first commit

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-  env: {
-    GOOGLE_CLIENT_ID:
-      "918867304291-40nfb87kbhf4vlloh0r95bev44i16qpf.apps.googleusercontent.com",
-    GOOGLE_CLIENT_SECRET: "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",
-  },
-};
+  reactStrictMode: true,
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
