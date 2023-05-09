2adead8b3297c4851999d5952434847c9ee0d0bc
Shubham Verma <vshubham799@gmail.com>
2023-05-09 01:18:12 -0700

    Changes done to add complete nextauth for google

--- pages/Profile.tsx
+++ pages/Profile.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useSession, signOut } from "next-auth/react";
+import { useSession, signOut, getSession } from "next-auth/react";
 export default function Profile() {
   const { data: session } = useSession();
 
@@ -8,6 +8,7 @@ export default function Profile() {
       {session ? (
         <>
           <h1>Welcome {session.user?.name}</h1>
+          <button onClick={() => signOut()}>Sign out</button>
         </>
       ) : (
         <>
@@ -17,3 +18,20 @@ export default function Profile() {
     </div>
   );
 }
+
+export const getServerSideProps = async (context) => {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/Login",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: { session },
+  };
+};
