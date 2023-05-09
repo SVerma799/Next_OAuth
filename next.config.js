256ea1ec41ac3919cedb3fad199c3b1d7e52d61a
Shubham Verma <vshubham799@gmail.com>
2023-05-08 23:26:54 -0700

    Changes initial commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  env: {
+    GOOGLE_CLIENT_ID:
+      "918867304291-40nfb87kbhf4vlloh0r95bev44i16qpf.apps.googleusercontent.com",
+    GOOGLE_CLIENT_SECRET: "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",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
